f54aacff0d7a305fdd50935061d945591aeab0f4
spianfet-max <spianfet@gmail.com>
2026-05-11 12:22:48 +0900

    main.jsx を更新

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx' // Standard relative path for files in the same folder
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
